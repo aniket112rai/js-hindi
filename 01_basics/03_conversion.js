0d8e1c078a8bb63d3789eb1d06f04e60ebddd9c2
aniket112rai <143783115+aniket112rai@users.noreply.github.com>
2023-10-02 14:29:55 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,40 @@
+let score = "33asd"
+console.log(typeof score)
+console.log(typeof(score))
+
+let value = Number(score)
+console.log(typeof value)
+console.log(value)
+console.log(typeof NaN)
+console.log(typeof null)
+console.table([null,"haha"])
+// "33"==>>33
+//"33asd" ==>> NaN
+// type of NaN = number
+// true=1, false = 0
+console.log("---------------------------------------------------")
+
+let IsLoggedIn = 1
+
+let BooleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(BooleanIsLoggedIn)
+console.log("---------------------------------------------------")
+let num=33
+let str=String(num)
+console.log(str)
+console.log(typeof str)
+console.log("---------------------------------------------------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
